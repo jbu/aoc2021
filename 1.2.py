1f3bc8a4cba7d15ac893c0be69937272d2b455e5
James Uther <jbu@users.noreply.github.com>
2021-12-01 11:51:54 +0000

    Create 1.2.py

---
+++ 1.2.py
@@ -0,0 +1,8 @@
+from more_itertools import pairwise, triplewise
+
+depths = open('in.txt').readlines()
+depths = [int(d) for d in depths]
+triples = [sum(d) for d in triplewise(depths)]
+pairs = pairwise(triples)
+pairs = [p[1] > p[0] for p in pairs]
+print(sum(pairs))
