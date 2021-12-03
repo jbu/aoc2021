13298ac5263175ce2b77e0d7c77f20a824532acc
James Uther <james.uther@oliverwyman.com>
2021-12-03 16:50:27 +0000

    1

--- p3_2.py
+++ p3_2.py
@@ -1,4 +1,3 @@
-from more_itertools import pairwise, triplewise
 from typing import List
 import numpy as np
 from io import StringIO
@@ -8,10 +7,21 @@ def do(measures: List[str]) -> int:
   >>> do(["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"])
   230
   """
-  d = np.array(list(''.join(measures))).astype(int).reshape(len(measures), len(measures[0]))
-  ɣ = int(''.join((np.mean(d, axis=0) > 0.5).astype(int).astype(str)), 2)
-  ε = int(''.join((np.mean(d, axis=0) < 0.5).astype(int).astype(str)), 2)
-  return ɣ * ε
+  i = 1 << len(measures[0])-1
+  m = [int(i, 2) for i in measures]
+  while i:
+    a = [x for x in m if x & i]
+    b = [x for x in m if not (x & i)]
+    print('a', [x&i for x in m])
+    print('b', [f"{(x&~(i-1)):b}" for x in m])
+    print('c', [f"{x:b}" for x in a], [f"{(x|i):b}" for x in b])
+    if len(a) > len(b):
+      m = a
+    else:
+      m = b
+    i = i >> 1
+
+  print(f"{i:b}, {m}")
 
 if __name__ == "__main__":
     import doctest
