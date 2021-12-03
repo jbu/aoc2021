56252a701b8188ff8e7c05d29119c84c67b61f0e
James Uther <jbu@users.noreply.github.com>
2021-12-03 23:38:31 +0000

    refactor

--- p3_2.py
+++ p3_2.py
@@ -1,30 +1,25 @@
-from typing import List
-import bisect
+from typing import List, Callable
+import operator
+
+def skriffle(measures: List[str], 
+             cmp_fn: Callable[[int, int], bool]) -> int:
+  i = 1 << len(measures[0])-1
+  m = [int(i, 2) for i in measures]
+  while i and len(m) > 1:
+    a = [x for x in m if x & i]
+    b = [x for x in m if not (x & i)]
+    m = a if cmp_fn(len(a), len(b)) else b
+    i = i >> 1
+  return m[0]
 
 def do(measures: List[str]) -> int:
   """
   >>> do(["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"])
   230
   """
-  i = 1 << len(measures[0])-1
-  m = [int(i, 2) for i in measures]
-  while i and len(m) > 1:
-    a = [x for x in m if x & i]
-    b = [x for x in m if not (x & i)]
-    m = a if len(a) >= len(b) else b
-    i = i >> 1
-  oxygen = m[0]
-
-  i = 1 << len(measures[0])-1
-  m = [int(i, 2) for i in measures]
-  while i and len(m) > 1:
-    a = [x for x in m if x & i]
-    b = [x for x in m if not (x & i)]
-    m = a if len(a) < len(b) else b 
-    i = i >> 1
-  co2 = m[0]
+  oxygen = skriffle(measures, operator.ge)
+  co2 = skriffle(measures, operator.lt)
   return oxygen * co2
-
 
 if __name__ == "__main__":
     import doctest
